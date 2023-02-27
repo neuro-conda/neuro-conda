d0b1ef32100a49315ed2deea9a0218d048e33a5e
Joscha Schmiedt <schmiedt@uni-bremen.de>
2023-02-27 09:10:01 +0100

    CHG: replace WinGet with registry check
    - Windows Server doesn't support WinGet so we simply check the
      registry for existing miniconda installations (fixes #10)

--- libexec/install.ps1
+++ libexec/install.ps1
@@ -3,22 +3,28 @@ $CondaInstallationDirectory = "$Env:userprofile\miniconda3"
 $MinicondaLatestUrl = "https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe"
 If ($Env:username -match " ") { $CondaInstallationDirectory = "$Env:public\miniconda3" }
 
-winget list --id Anaconda.Miniconda3 | out-null
-If ($LASTEXITCODE -ne 0)
-{
+
+function Find-Miniconda {           
+    $software = "Miniconda3"
+    return (((Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*).DisplayName -Match $software).Length -gt 0) -or `
+        (((Get-ItemProperty HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*).DisplayName -Match $software).Length -gt 0)
+}
+
+$CondaIsInstalled = Find-Miniconda
+
+If (-not $CondaIsInstalled) {
     Write-Host "Downloading miniconda3"
     Invoke-WebRequest $MinicondaLatestUrl -OutFile $Env:temp\Miniconda3-latest-Windows-x86_64.exe
     Invoke-Expression "$Env:temp\Miniconda3-latest-Windows-x86_64.exe /RegisterPython=1 /S /InstallationType=JustMe /D=$CondaInstallationDirectory"
     Write-Host "Installed miniconda into $CondaInstallationDirectory"
     Invoke-Expression "$CondaInstallationDirectory\shell\condabin\conda-hook.ps1"
     conda init powershell
-} Else {Write-Host "miniconda3 is already installed"}
+}
+Else { Write-Host "miniconda3 is already installed" }
 
-If (-not (Get-Command "conda" -errorAction SilentlyContinue))
-{
+If (-not (Get-Command "conda" -errorAction SilentlyContinue)) {
     throw "Conda is installed but not available in this PowerShell. Please continue with the neuro-conda installation from a PowerShell with conda activated."
 }
-
 
 # update conda
 conda update -n base conda -c defaults -y
