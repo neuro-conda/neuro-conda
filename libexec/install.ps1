28557794cd3b0c025574717d39981e7eb5dc3432
Stefan Fuertinger <stefan.fuertinger@esi-frankfurt.de>
2023-04-27 12:43:22 +0200

    CHG: Use single quotes in & expression
    - try to use single quotes in PS command eval expression
    
     On branch minimal_tests
     Changes to be committed:
    	modified:   libexec/install.ps1

--- libexec/install.ps1
+++ libexec/install.ps1
@@ -28,7 +28,7 @@ If ((-not $CondaIsInstalled) -or ($Env:ncCI)){
     Invoke-WebRequest $MinicondaLatestUrl -OutFile $Env:temp\Miniconda3-latest-Windows-x86_64.exe
     Invoke-Expression "$Env:temp\Miniconda3-latest-Windows-x86_64.exe /RegisterPython=1 /S /InstallationType=JustMe /D=$CondaInstallationDirectory"
     Write-Host "Installed miniconda into $CondaInstallationDirectory"
-    & "$CondaInstallationDirectory\shell\condabin\conda-hook.ps1"
+    & '$CondaInstallationDirectory\shell\condabin\conda-hook.ps1'
     conda init powershell
 }
 Else { Write-Host "miniconda3 is already installed" }
