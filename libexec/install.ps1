1b9e731b083c114527ecaa4b94064cacfa93f5da
Stefan Fuertinger <stefan.fuertinger@esi-frankfurt.de>
2023-05-15 16:57:14 +0200

    CHG: Check if download succeeds
    - maybe things go wrong already there
    
     On branch minimal_tests
     Changes to be committed:
    	modified:   libexec/install.ps1

--- libexec/install.ps1
+++ libexec/install.ps1
@@ -26,6 +26,7 @@ $CondaIsInstalled = Find-Miniconda
 If ((-not $CondaIsInstalled) -or ($Env:ncCI)){
     Write-Host "Downloading miniconda3"
     Invoke-WebRequest $MinicondaLatestUrl -OutFile $Env:temp\Miniconda3-latest-Windows-x86_64.exe
+    Invoke-Expression "ls $Env:temp"
     Invoke-Expression "$Env:temp\Miniconda3-latest-Windows-x86_64.exe /RegisterPython=0 /InstallationType=JustMe /D=$CondaInstallationDirectory"
     Write-Host "Installed miniconda into $CondaInstallationDirectory"
     Invoke-Expression "ls C:\Users\runneradmin"
