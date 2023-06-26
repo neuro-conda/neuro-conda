631cfd2753dcbb0ae75c17094faaa9f2e4c95b20
Stefan Fuertinger <stefan.fuertinger@esi-frankfurt.de>
2023-06-26 15:43:47 +0200

    CHG: Try different installation dir
     Changes to be committed:
    	modified:   libexec/install.ps1

--- libexec/install.ps1
+++ libexec/install.ps1
@@ -27,7 +27,7 @@ If ((-not $CondaIsInstalled) -or ($Env:ncCI)){
     Write-Host "Downloading miniconda3"
     Invoke-WebRequest $MinicondaLatestUrl -OutFile $Env:temp\Miniconda3-latest-Windows-x86_64.exe
     Invoke-Expression "ls $Env:temp"
-    Start-Process -FilePath "$Env:temp\Miniconda3-latest-Windows-x86_64.exe" -ArgumentList "/InstallationType=JustMe","/D=$CondaInstallationDirectory"
+    Start-Process -FilePath "$Env:temp\Miniconda3-latest-Windows-x86_64.exe" -ArgumentList "/InstallationType=JustMe","/D=$Env:temp\miniconda3"
     # & "$Env:temp\Miniconda3-latest-Windows-x86_64.exe /InstallationType=JustMe /D=$CondaInstallationDirectory"
     Write-Host "Installed miniconda into $CondaInstallationDirectory"
     Invoke-Expression "ls C:\Users\runneradmin"
