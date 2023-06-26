cdcd7fec4a2a140ee08032b0ff4947113870af49
Stefan Fuertinger <stefan.fuertinger@esi-frankfurt.de>
2023-06-26 16:47:47 +0200

    test

--- libexec/install.ps1
+++ libexec/install.ps1
@@ -28,7 +28,9 @@ If ((-not $CondaIsInstalled) -or ($Env:ncCI)){
     Write-Host "Downloading miniconda3"
     Invoke-WebRequest $MinicondaLatestUrl -OutFile $Env:temp\Miniconda3-latest-Windows-x86_64.exe
     Invoke-Expression "ls $Env:temp"
-    Invoke-Expression "$Env:temp\Miniconda3-latest-Windows-x86_64.exe /InstallationType=JustMe /S /D=$CondaInstallationDirectory"
+    $Command = '$Env:temp\Miniconda3-latest-Windows-x86_64.exe /InstallationType=JustMe /S /D=$CondaInstallationDirectory'
+    Invoke-Expression $Command
+    # Invoke-Expression "$Env:temp\Miniconda3-latest-Windows-x86_64.exe /InstallationType=JustMe /S /D=$CondaInstallationDirectory"
     # Write-Host "$Env:temp\Miniconda3-latest-Windows-x86_64.exe /S /InstallationType=JustMe /D=$CondaInstallationDirectory"
     # Start-Process -FilePath "$Env:temp\Miniconda3-latest-Windows-x86_64.exe" -ArgumentList "/InstallationType=JustMe","/S","/D=$CondaInstallationDirectory" -PassThru -Wait
     # (& "$Env:temp\Miniconda3-latest-Windows-x86_64.exe" "/InstallationType=JustMe" "/S" "/D=$CondaInstallationDirectory")  | Out-String | Invoke-Expression
