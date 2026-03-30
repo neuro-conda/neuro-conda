a33b5561d1cd275c2778c79c6f0d384b0999a743
Stephanie Reilly <reillys@esi-wsit020.esi.local>
2026-03-30 11:23:49 +0200

    CHG: editing the pkgMap dictionary to match the environment files

--- tests/test_packages.py
+++ tests/test_packages.py
@@ -43,20 +43,32 @@ def test_imports():
     if os.getenv("ncCI"):
         ignorePkgs += ["invertmeeg", "torchaudio", "tensorflow"]
 
+    
     # Packages whose name does not correspond to their Python module name
-    pkgMap = {
-        "esi-acme": "acme",
-        "invertmeeg": "invert",
-        "opencv-python": "cv2",
-        "opencv": "cv2",
-        "open-ephys-python-tools": "open_ephys",
-        "pep8-naming": "pep8ext_naming",
-        "pybids": "bids",
-        "pywavelets": "pywt",
-        "scikit-image": "skimage",
-        "scikit-learn": "sklearn",
-        "scikit-learn-intelex": "sklearnex",
-    }
+    if current_machine == "ppc64le":
+        pkgMap = {
+            "esi-acme": "acme",
+            "opencv": "cv2",
+            "open-ephys-python-tools": "open_ephys",
+            "pep8-naming": "pep8ext_naming",
+            "pybids": "bids",
+            "pywavelets": "pywt",
+            "scikit-image": "skimage",
+            "scikit-learn": "sklearn",
+        }
+    else:
+        pkgMap = {
+            "esi-acme": "acme",
+            "invertmeeg": "invert",
+            "opencv-python": "cv2",
+            "open-ephys-python-tools": "open_ephys",
+            "pep8-naming": "pep8ext_naming",
+            "pybids": "bids",
+            "pywavelets": "pywt",
+            "scikit-image": "skimage",
+            "scikit-learn": "sklearn",
+            "scikit-learn-intelex": "sklearnex",
+        }
 
     # Split off pip-installed packages since those contain additional
     # platform/arch restrictions that have to be dealt with separately
